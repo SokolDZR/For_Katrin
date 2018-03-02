0da0311f2e7e8f49ba3e822f875eff3c986d2a30
SokolDZR <antonyuk-daniil@mail.ru>
2018-03-02 19:10:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/student2/list/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.student2.list;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long id) {
+                String country = (String) adapterView.getItemAtPosition(i);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://en.wikipedia.org/wiki/" + country.replace(" ", "_")));
+                startActivity(browserIntent);
+            }
+        });
+    }
+}
